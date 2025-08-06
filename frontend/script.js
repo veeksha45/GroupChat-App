31bf524f5c798ca74ccf2c176bab9a6ad4dd3c4e
veeksha45 <veeksha45@gmail.com>
2025-08-06 13:20:16 +0530

    frontend

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+document.getElementById('signupForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const password = document.getElementById('password').value;
+
+  const res = await fetch('http://localhost:3000/api/auth/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, phone, password })
+  });
+
+  const data = await res.json();
+  document.getElementById('message').textContent = data.message;
+});
