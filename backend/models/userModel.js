6152c9ef5f9af942478b601cf01ae649b25fa136
veeksha45 <veeksha45@gmail.com>
2025-08-06 13:24:48 +0530

    Add files via upload

---
+++ backend/models/userModel.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  phone: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false }
+});
+
+module.exports = User;
